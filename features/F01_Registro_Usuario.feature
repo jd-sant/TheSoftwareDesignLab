6be66c9d13da2a4fb0a4aa11411e23727f062ccf
Diego Santamaria <jd.santamariab1@uniandes.edu.co>
2024-11-06 13:24:27 -0500

    First scenario to create admin user

---
+++ features/F01_Registro_Usuario.feature
@@ -0,0 +1,19 @@
+Feature: Registro y uso usuario administrador
+
+@user1 @web
+Scenario: E001 - Como administrador de la plataforma, quiero crear un usuario para administrar el sitio
+  Given I navigate to page "http://localhost:3001/ghost"
+  And I wait for 5 seconds
+  When I enter site title "<SITE_TITLE>"
+  And I wait for 1 seconds
+  When I enter full name "<FULL_NAME>"
+  And I wait for 1 seconds
+  When I enter email "<EMAIL>"
+  And I wait for 1 seconds
+  When I enter password "<PASSWORD>"
+  And I wait for 1 seconds
+  When I click on submit button
+  And I wait for 10 seconds
+  Then I should see landing page for first time
+  And I wait for 5 seconds
+  And I send a signal to user 2 containing "User created"
--- features/web/step_definitions/step.js
+++ features/web/step_definitions/step.js
@@ -1,1 +1,35 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
+const assert = require('assert');
+
+// When Conditions
+When('I enter site title {kraken-string}', async function (title) {
+    let element = await this.driver.$('#blog-title');
+    return await element.setValue(title);
+});
+
+When('I enter full name {kraken-string}', async function (name) {
+    let element = await this.driver.$('#name');
+    return await element.setValue(name);
+});
+
+When('I enter email {kraken-string}', async function (email) {
+    let element = await this.driver.$('#email');
+    return await element.setValue(email);
+});
+
+When('I enter password {kraken-string}', async function (password) {
+    let element = await this.driver.$('#password');
+    return await element.setValue(password);
+});
+
+When('I click on submit button', async function () {
+    let element = await this.driver.$('#ember4');
+    return await element.click();
+});
+
+// Then Conditions
+Then('I should see landing page for first time', async function () {
+    let element = await this.driver.$('.gh-onboarding-header h2');
+    let text = await element.getText();
+    assert.strictEqual(text, 'Let’s get started!');
+});
