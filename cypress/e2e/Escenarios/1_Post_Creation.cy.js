2abcf161ece8520204b4fd6082fc2d89cfc0dbec
Diego Santamaria <jd.santamariab1@uniandes.edu.co>
2024-11-30 20:39:12 -0500

    Reestructuración carpetas

--- cypress/e2e/Escenarios/1_Post_Creation.cy.js
+++ cypress/e2e/Escenarios/1_Post_Creation.cy.js
@@ -4,13 +4,18 @@ import { thenSteps } from '../../steps/ThenSteps';
 
 describe('Post Creation', () => {
     let baseData;
+    let mockData;
     beforeEach(() => {
-        cy.fixture('postSchema.json').then((data) => {
-            const randomIndex = Math.floor(Math.random() * data.length);
-            baseData = data[randomIndex]; // Asignar los datos a la variable baseData
-        });
-        Cypress.Screenshot.defaults({
-            disableTimersAndAnimations: false,
+        // Realizar la petición a la API de Mockaroo para obtener 1000 registros
+        cy.request({
+            method: 'GET',
+            url: 'https://my.api.mockaroo.com/postSchema?key=9f987470',
+        }).then((response) => {
+            // Asegurarse de que la respuesta sea exitosa
+            expect(response.status).to.eq(200);
+            mockData = response.body; // Asignamos la respuesta a la variable mockData
+            const randomIndex = Math.floor(Math.random() * mockData.length);
+            baseData = mockData[randomIndex];
         });
         // Given the user has navigated to the Ghost site
         givenSteps.givenNavigateToTheSite();
@@ -81,5 +86,33 @@ describe('Post Creation', () => {
         whenSteps.whenCreateAndPublishPostURL(baseData); 
         // Then the user should be the post published
         thenSteps.thenSeePostPublishedURL(baseData);
+    });
+
+    it('PA0##-C - Create a empty post', () => {
+        // When the user creates a empty post
+        whenSteps.whenCreateEmptyPost(baseData);
+        // Then the user shouldn't see the publish button
+        thenSteps.thenPublishButtonUnavailable();
+    });
+
+    it('PA0##-C - Create an existing post', () => {
+        // When the user creates a post
+        whenSteps.whenCreateAndPublishPost(baseData); 
+        // Then the user shouldn't be able to publish the same post again
+        thenSteps.thenCantPublishExistingPost(baseData);
+    });
+
+    it('PA0##-C - Update post published date', () => {
+        // When a user edit a post and update the published date
+        whenSteps.whenUpdatePostPublishedDate();
+        // Then the user should see the updated button enable
+        thenSteps.thenUpdatedButtonEnable();
+    });
+
+    it('PA0##-C - Creat a post with a emoji as url', () => {
+        // When the user creates a post with a emoji as url
+        whenSteps.whenCreateAndPublishPostURLEmoji(baseData);
+        // Then the user shouldn't be able to publish the post
+        thenSteps.thenCantPublishExistingPost(baseData);
     });
 });
